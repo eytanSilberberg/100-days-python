1c1d9e5fcb2ddcbbb7aeda450c3e6101e5c72086
Eytan Silberberg <102313688+eytanSilberberg@users.noreply.github.com>
2023-09-03 22:29:22 +0300

    finised calculator

--- day10-more-function/calculator.py
+++ day10-more-function/calculator.py
@@ -3,56 +3,54 @@ from logo import logo
 
 def add(prev_num, user_input):
     answer = prev_num+user_input
-    print(f'{prev_num}+{user_input}={answer}')
     return answer
 
 
 def sub(prev_num, user_input):
     answer = prev_num-user_input
-    print(f'{prev_num}-{user_input}={answer}')
     return answer
 
 
 def mult(prev_num, user_input):
     answer = prev_num*user_input
-    print(f'{prev_num}*{user_input}={answer}')
     return answer
 
 
 def divide(prev_num, user_input):
     answer = prev_num/user_input
-    print(f'{prev_num}/{user_input}={answer}')
     return answer
+
+
+operations = {
+    '+': add,
+    '-': sub,
+    '*': mult,
+    '/': divide
+}
 
 
 def calculate():
     print(logo)
     is_calculating = True
-    total = int(input('Select a number '))
+    total = float(input('Select a number '))
     while is_calculating:
         operation = input('Select the Operation you\'d like to do +,-,* or / ')
-        user_input = int(input('Select a number '))
-        if (operation == '+'):
-            total = add(total, user_input)
-        elif (operation == '-'):
-            total = sub(total, user_input)
-        elif (operation == '*'):
-            total = mult(total, user_input)
-        elif (operation == '/'):
-            total = divide(total, user_input)
-        else:
-            print('Please choose one of the operations ')
-        to_continue = ''
-        while to_continue != 'y' and to_continue != 'n':
-            to_continue = input("Do you wish to continue? Y or N ")
-            to_continue = to_continue.lower()
-            print(to_continue)
-            if to_continue == 'y':
-                is_calculating = True
-            elif to_continue == 'n':
-                is_calculating = False
+        user_input = float(input('Select a number '))
+        is_valid_operation = False
+        while not is_valid_operation:
+            if operation != '+' and operation != '-' and operation != '*' and operation != '/':
+                print('Please choose one of the operations ')
             else:
-                print('Pick a valid option')
+                answer = operations[operation](total, user_input)
+                print(f'{total} {operation} {user_input} = {answer}')
+                total = answer
+                is_valid_operation = True
+        wish_to_continue = input(
+            'Wish to continue? Please choose Y or N').lower()
+        if wish_to_continue == 'y':
+            is_calculating = True
+        elif wish_to_continue == 'n':
+            is_calculating = False
 
     return total
 
