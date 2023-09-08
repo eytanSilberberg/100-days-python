bb2d1f94f20164ca3c4f10b1e91cc74f191a7f02
Eytan Silberberg <102313688+eytanSilberberg@users.noreply.github.com>
2023-09-08 16:28:17 +0300

    higher and lower game

--- day13-debugging/debugging-challenges.py
+++ day13-debugging/debugging-challenges.py
@@ -20,3 +20,17 @@ if year % 4 == 0:
         print("Leap year.")
 else:
     print("Not leap year.")
+
+# EX3
+for number in range(1, 101):
+    #   prev code was if number % 3 == 0 or number % 5 == 0:
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+        # prev code was if number % 3 == 0:
+    elif number % 3 == 0:
+        print("Fizz")
+        # prev code was if number % 5 == 0:
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print([number])
---
+++ day14-higer-or-lower/higher-or-lower.py
@@ -0,0 +1,61 @@
+from art import logo, vs
+from game_data import data
+import random
+print(logo)
+
+
+# Todo1 - create all variables that define the game such as- score, is_playing
+
+score = 0
+is_playing = True
+
+random.shuffle(data)
+curr_page = data.pop()
+curr_compare = data.pop()
+did_win = False
+# Todo2- Define what is the condition for game to keep going
+while is_playing:
+    print('Who has more followers? ')
+    print(f'{curr_page["name"]}, {curr_page["description"]} from {curr_page["country"]}. This profile has {curr_page["follower_count"]}')
+    print(vs)
+    print(
+        f'{curr_compare["name"]}, {curr_compare["description"]} from {curr_compare["country"]}')
+    answer = input("Who have more followers? A or B? ").lower()
+    winner = None
+    print(f'{curr_page["name"]} has {curr_page["follower_count"]} followers vs {curr_compare["name"]} has {curr_compare["follower_count"]} followers')
+    if answer == 'a' or answer == 'b':
+        if curr_page["follower_count"] > curr_compare["follower_count"]:
+            if answer == 'a':
+                if len(data) > 0:
+                    curr_compare = data.pop()
+                else:
+                    is_playing = False
+                    did_win = True
+
+            elif answer == 'b':
+                is_playing = False
+        elif curr_page["follower_count"] < curr_compare["follower_count"]:
+            if answer == 'a':
+                is_playing = False
+            elif answer == 'b':
+                if len(data) > 0:
+                    temp_page = curr_compare
+                    curr_page = temp_page
+                    curr_compare = data.pop()
+                else:
+                    is_playing = False
+                    did_win = True
+        else:
+            if answer == 'a':
+                curr_compare = data.pop()
+            elif answer == 'b':
+                temp_page = curr_compare
+                curr_page = temp_page
+                curr_compare = data.pop()
+    else:
+        print('Please pick a valid option ')
+
+if not is_playing and did_win:
+    print('You won!')
+else:
+    print('You lose!')
