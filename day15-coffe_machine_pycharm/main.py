64ae12ed6fba99a3fea610e1954455a46e21eee3
Eytan Silberberg <102313688+eytanSilberberg@users.noreply.github.com>
2023-09-09 14:24:01 +0300

    refcatored

--- day15-coffe_machine_pycharm/main.py
+++ day15-coffe_machine_pycharm/main.py
@@ -35,10 +35,7 @@ report = {
     "money": 0
 }
 
-# 1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
-# a. Check the user’s input to decide what to do next.
-# b. The prompt should show every time action has completed, e.g. once the drink is
-# dispensed. The prompt should show again to serve the next customer.
+
 is_activated = True
 user_options = ['espresso', 'latte', 'cappuccino', 'off', 'report']
 
@@ -49,7 +46,6 @@ def subtract_resources(ingredients):
 
 
 def charge_amount(choice):
-    # b. Remember that quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
     price = choice["cost"]
     print(f'Drink costs {price}$')
     coins = {
@@ -106,6 +102,10 @@ while is_activated:
                     subtract_resources(choice["ingredients"])
                     print("Enjoy your coffe!")
 
+# 1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
+# a. Check the user’s input to decide what to do next.
+# b. The prompt should show every time action has completed, e.g. once the drink is
+# dispensed. The prompt should show again to serve the next customer.
 
 # 2. Turn off the Coffee Machine by entering “off” to the prompt.
 # a. For maintainers of the coffee machine, they can use “off” as the secret word to turn off
